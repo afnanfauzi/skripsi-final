7d0f5628c27c6dc6b642462721bf53aa2252cb95
afnanfauzi <afnanfauzihidayat@gmail.com>
2021-05-05 21:01:26 +0700

    Perubahan halaman dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,5 +1,12 @@
 <!-- jQuery -->
 <script src="{{ asset('dashboard/vendors/jquery/dist/jquery.min.js') }}"></script>
+{{-- Custom css cards --}}
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<link href="{{ asset('dashboard/build/css/blog/bootstrap-extended.min.css') }}" rel="stylesheet">
+<link href="{{ asset('dashboard/build/css/blog/colors.min.css') }}" rel="stylesheet">
+<link href="{{ asset('dashboard/build/css/blog/style.min.css') }}" rel="stylesheet">
+<link href="{{ asset('dashboard/build/css/blog/bootstrap.min.css') }}" rel="stylesheet">
+
 @extends('layouts.master')
 @section('csrf')
 <meta name="csrf-token" content="{{ csrf_token() }}">    
@@ -7,36 +14,91 @@
 @section('title', 'Dashboard - Pimpinan Daerah Muhammadiyah Sragen')
 @section('content')
 <div class="col-12">
-    <!-- top tiles -->
-    <div class="row-nan" style="display: inline-block;" >
-    <div class="tile_count">
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-briefcase"></i> Total Kegiatan</span>
-        <div class="count green">{{ $kegiatan }}</div>
-      </div>
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-users"></i> Total Anggota</span>
-        <div class="count green">{{ $anggota }}</div>
-      </div>
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-building-o"></i> Total Unit</span>
-        <div class="count green">{{ $unit }}</div>
-      </div>
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-institution"></i> Total Cabang</span>
-        <div class="count green">{{ $cabang }}</div>
-      </div>
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-university"></i> Total Ranting</span>
-        <div class="count green">{{ $ranting }}</div>
-      </div>
-      <div class="col-md-2 col-sm-4  tile_stats_count">
-        <span class="count_top"><i class="fa fa-dribbble"></i> Total Artikel</span>
-        <div class="count green">{{ $artikel }}</div>
-      </div>
+    <div class="grey-bg container-fluid">
+      <section id="minimal-statistics">
+        <div class="row">
+          <div class="col-12 mt-3 mb-1">
+            <h4 class="text-uppercase">Dashboard</h4>
+          </div>
+        </div>
+      
+        <div class="row">
+          <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="media-body text-left">
+                      <h3 class="danger">{{ $kegiatan }}</h3>
+                      <span>Total Kegiatan</span>
+                    </div>
+                    <div class="align-self-center">
+                      <i class="icon-briefcase danger font-large-2 float-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="media-body text-left">
+                      <h3 class="success">{{ $anggota }}</h3>
+                      <span>Total Anggota</span>
+                    </div>
+                    <div class="align-self-center">
+                      <i class="icon-users success font-large-2 float-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      
+          <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="media-body text-left">
+                      <h3 class="warning">{{ $unit }}</h3>
+                      <span>Total Unit</span>
+                    </div>
+                    <div class="align-self-center">
+                      <i class="icon-star warning font-large-2 float-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="media-body text-left">
+                      <h3 class="primary">{{ $cabang }}</h3>
+                      <span>Total Cabang</span>
+                    </div>
+                    <div class="align-self-center">
+                      <i class="icon-home primary font-large-2 float-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
     </div>
-  </div>
-    <!-- /top tiles -->
+
+
+
 </div>
 
 @endsection
